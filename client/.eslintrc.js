f2d89a7e2511de9410a30f43cd454ee4b3e4d193
kiran <kiran.s.dhawan@gmail.com>
2025-07-28 17:06:57 +0530

    Update ESLint configuration to disable specific rules and modify build script for better compatibility

---
+++ client/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // This disables all ESLint rules
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'no-unused-vars': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'eqeqeq': 'off',
+    'no-use-before-define': 'off',
+    'no-dupe-keys': 'off'
+  }
+};
